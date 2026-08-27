8b5b04fabe76c419a5b4777eab90cd4ced0b3306
djdenicore <pardaniil0@gmail.com>
2026-08-27 04:48:24 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,5 +0,0 @@
-import sounddevice as sd
-
-print("--- Все доступные аудиоустройства ---")
-for i, dev in enumerate(sd.query_devices()):
-    print(f"[{i}] {dev['name']} (Входов: {dev['max_input_channels']}, Выходов: {dev['max_output_channels']})")
